d2f707d1bf4102d6fd0a393fe68e842bd94afdae
jasonlam <jlam@palo-it.com>
2018-12-05 13:24:44 +0800

    Add script

---
+++ dockercopyfiles.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+#${1} src host folder
+#${2} dest docker
+#${3} dest folder
+#${4} template
+curl -s -o tmp-exclude.txt https://raw.githubusercontent.com/jasonlamkk/jenkins-shall-copy-what-files/master/${4}/exclude.txt && arrExclude=$(< tmp-exclude.txt ) && rm tmp-exclude.txt
+
+arrFiles=$(ls ${1})
+for f in $arrFiles
+do
+    if [[ ${arrFiles[*]} =~ $f ]]; then
+        echo "Skip : ${f}"
+    else
+        docker cp ${1}/${f} ${2}:${3}/${f}
+    fi
+done
